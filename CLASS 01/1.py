9fc29eaa5c42d7848dda160f55f24e2ff55a32d7
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2023-12-23 17:27:31 +0500

    Add files via upload

---
+++ CLASS 01/1.py
@@ -0,0 +1,20 @@
+'''print('Programming Funndamentals101')
+print("Hello World")
+asim='hello this is asim'
+hujur=28
+print(hujur)
+print(asim)
+
+print(35)
+
+
+
+a=10
+b='Hello'
+c='99.01'
+d=87.789
+print(d)
+'''
+
+
+
---
+++ CLASS 01/calculator.py
@@ -0,0 +1,19 @@
+'''a=eval(input('Enter the number '))
+b=eval(input('Enter the second number '))
+c=a+b
+d=a-b
+e=a*b
+f=a//b
+
+print('Addition',c)
+print('Subtraction',d)
+print('Multiplication',e)
+print('Division',f)'''
+
+a=input('Enter you name ')
+print(a)
+a=input('Enter you father name')
+
+
+
+
