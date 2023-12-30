787f1bd50fd346bacd78f13901b1c820eed34744
Hasan Raza <65623858+Hasan132002@users.noreply.github.com>
2023-12-30 17:55:37 +0500

    Add files via upload

---
+++ Class 02/1.py
@@ -0,0 +1,34 @@
+#ai_description="Artificial Intelligence (AI) is transforming industry and reshaping the Future"
+#print(ai_description.count('AI'))
+
+#maths=eval(input("Enter the Student maths number "))
+#physics=eval(input("Enter the Student phy number "))
+#chemistry=eval(input("Enter the Student chem number "))
+#english=eval(input("Enter the Student engl number "))
+#computer=eval(input("Enter the Student comp number "))
+#total_marks=maths+physics+chemistry+english+computer
+#percentage=(total_marks/500)*100
+'''percentage=eval(input('Enter pecentage'))
+name=input('enter name ' )
+print(f"you name is {name} percentage us {percentage:.2f}")'''
+#percentage=eval(input("Enter"))
+'''if(percentage>=90):
+    print('A+')
+elif(percentage<90 and percentage>=80):
+    print('A')
+elif(percentage<80 and percentage>=70):
+    print('B')
+elif(percentage<70 and percentage>=60):
+    print('C')
+elif(percentage<60 and percentage>=50):
+    print('D')
+else:
+    print('F')
+'''
+
+
+a=6
+a//=3
+print(a)
+
+
